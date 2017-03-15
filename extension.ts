4ae106f219e342dbd8744c02de5d8be977d9e497
Alessandro Fragnani <alefragnani@hotmail.com>
2017-03-14 21:25:52 -0300

    Closes #8

--- extension.ts
+++ extension.ts
@@ -1,110 +1,111 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'; 
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) { 
+export function activate(context: vscode.ExtensionContext) {
 
-	// register commands
-	vscode.commands.registerCommand('tagged-comment.tag', () => tag(false));
-	vscode.commands.registerCommand('tagged-comment.reTag', () => reTag(false));
-	vscode.commands.registerCommand('tagged-comment.tagAbove', () => tag(true));
-	vscode.commands.registerCommand('tagged-comment.reTagAbove', () => reTag(true));
-	
-	function reTag(lineAbove: boolean) {
-		let lastTagged = context.globalState.get('lastTagged', '');
-		// console.log('lastTagged: ', lastTagged);
-		insertTextInEditor(lastTagged, lineAbove);
-	};
+    // register commands
+    vscode.commands.registerCommand("tagged-comment.tag", () => tag(false));
+    vscode.commands.registerCommand("tagged-comment.reTag", () => reTag(false));
+    vscode.commands.registerCommand("tagged-comment.tagAbove", () => tag(true));
+    vscode.commands.registerCommand("tagged-comment.reTagAbove", () => reTag(true));
 
-	function tag(lineAbove: boolean) {
-		let lastEnteredText = context.globalState.get('lastEnteredText', '');
+    function reTag(lineAbove: boolean) {
+        let lastTagged = context.globalState.get("lastTagged", "");
+        insertTextInEditor(lastTagged, lineAbove);
+    }
 
-		// configure the InputBox				
-		var ibo = <vscode.InputBoxOptions>{
-			prompt: "Add a TAG",
-			placeHolder: "the placeholder",
-			value: lastEnteredText
-		}
+    function tag(lineAbove: boolean) {
+        let lastEnteredText = context.globalState.get("lastEnteredText", "");
 
-		// as the user
-		vscode.window.showInputBox(ibo)
-			.then(text => {
-				// console.log("You typed: " + text);
-				if (typeof text == 'undefined') {
-					return;
-				}
+        // configure the InputBox				
+        let ibo = <vscode.InputBoxOptions> {
+            prompt: "Add a TAG",
+            placeHolder: "the placeholder",
+            value: lastEnteredText
+        };
 
-				context.globalState.update('lastEnteredText', text);					
+        // as the user
+        vscode.window.showInputBox(ibo)
+            .then(text => {
+                // console.log("You typed: " + text);
+                if (typeof text === "undefined") {
+                    return;
+                }
 
-				// creates the tag
-				var date = new Date();
-				var day, month, year;
+                context.globalState.update("lastEnteredText", text);
 
-				if (date.getDate().toString().length == 1) {
-					day = "0" + date.getDate()
-				} else {
-					day = date.getDate()
-				}
+                // creates the tag
+                let date = new Date();
+                let day: string;
+                let month: string;
+                let year: string;
 
-				if ((date.getMonth() + 1).toString().length == 1) {
-					month = "0" + (date.getMonth() + 1)
-				} else {
-					month = date.getMonth() + 1
-				}
+                if (date.getDate().toString().length === 1) {
+                    day = "0" + date.getDate();
+                } else {
+                    day = date.getDate().toString();
+                }
 
-				year = date.getFullYear();
-				
-				//
-				var tagString = vscode.workspace.getConfiguration('tagged').get('tagString', '// #day/#month/#year - TAG: #enteredText');
+                if ((date.getMonth() + 1).toString().length === 1) {
+                    month = "0" + (date.getMonth() + 1);
+                } else {
+                    month = (date.getMonth() + 1).toString();
+                }
 
-				// console.log("tagstring: " + tagString);
+                year = date.getFullYear().toString();
 
-				var str = tagString;
-				str = str.replace("#day", day);
-				str = str.replace("#month", month);
-				str = str.replace("#year", year);
-				str = str.replace("#enteredText", text);
+                //
+                let tagString = vscode.workspace.getConfiguration("tagged").get("tagString", "// #day/#month/#year - TAG: #enteredText");
 
-				// console.log("str: " + str);
+                // console.log("tagstring: " + tagString);
 
-				context.globalState.update('lastTagged', str);
+                let str = tagString;
+                str = str.replace("#day", day);
+                str = str.replace("#month", month);
+                str = str.replace("#year", year);
+                str = str.replace("#enteredText", text);
 
-				insertTextInEditor(str, lineAbove);
-			});
-	};
-	
-	function getIndentText(line: string): string {
-		let pos: number = line.search(/\S/);
-		return line.substr(0, pos);	
-	}
+                // console.log("str: " + str);
 
-	function insertTextInEditor(text: string, lineAbove: boolean) {
-		let selection: vscode.Selection;
-		let startLine: number;
-		let startCharacter: number;
-		
-		selection = vscode.window.activeTextEditor.selection;
-		startLine = selection.start.line;
-		
-		if (!lineAbove) {
-			startCharacter = selection.start.character;
-		} else {
-			let lineText: string = vscode.window.activeTextEditor.document.lineAt(selection.start.line).text;
-			text = getIndentText(lineText).concat(text).concat("\r");
-			startCharacter = 0;
-		}
+                context.globalState.update("lastTagged", str);
 
-		let editor = vscode.window.activeTextEditor;
-		editor.edit((editBuilder) => {
-			let pos = new vscode.Position(startLine, startCharacter);
-			editBuilder.insert(pos, text);
-		}).then(() => {
-			// console.log('Edit applied!');
-		}, (err) => {
-			// console.log('Edit rejected!');
-			// console.error(err);
-		});
-	}
+                insertTextInEditor(str, lineAbove);
+            });
+    }
+
+    function getIndentText(line: string): string {
+        let pos: number = line.search(/\S/);
+        return line.substr(0, pos);
+    }
+
+    function insertTextInEditor(text: string, lineAbove: boolean) {
+        let selection: vscode.Selection;
+        let startLine: number;
+        let startCharacter: number;
+
+        selection = vscode.window.activeTextEditor.selection;
+        startLine = selection.start.line;
+
+        if (!lineAbove) {
+            startCharacter = selection.start.character;
+        } else {
+            let lineText: string = vscode.window.activeTextEditor.document.lineAt(selection.start.line).text;
+            text = getIndentText(lineText).concat(text).concat("\r");
+            startCharacter = 0;
+        }
+
+        let editor = vscode.window.activeTextEditor;
+        editor.edit((editBuilder) => {
+            let pos = new vscode.Position(startLine, startCharacter);
+            editBuilder.insert(pos, text);
+        }).then(() => {
+            // console.log('Edit applied!');
+        }, (err) => {
+            // console.log('Edit rejected!');
+            // console.error(err);
+        });
+    }
 }
